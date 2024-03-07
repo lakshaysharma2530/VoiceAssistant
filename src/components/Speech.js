4672caec425c92327340c0d856731d7e4d17e4a4
lakshaysharma2530 <136422387+lakshaysharma2530@users.noreply.github.com>
2024-03-07 23:43:16 +0530

    first commit

---
+++ src/components/Speech.js
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from 'react';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import listen from "./listen.svg"
+import "./mic.css"
+import listeningAnim from "./listening-anim.mp4"
+import axios from 'axios';
+import { API } from '../config';
+import { useSpeechSynthesis } from 'react-speech-kit';
+const Dictaphone = () => {
+  const [reply,setReply]=useState("");
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition
+  } = useSpeechRecognition();
+  axios.defaults.baseURL=API;
+  const [res,setRes]=useState()
+  const {speak}=useSpeechSynthesis();
+
+  
+
+    useEffect(()=>{
+      const fetchdata= async()=>{
+      if(transcript!=="" && listening===false){
+
+        try {
+          const query={transcript}.transcript
+          const { data } = await axios.get(`/ask?query=${query}`);
+          setRes(data)
+
+        } catch (error) {
+          console.error("Error fetching data:", error);
+
+      }}}
+      
+      fetchdata()
+      
+    },[listening,transcript])
+
+    useEffect(()=>{
+      const speech=()=>{
+        if (res && res.answer !== undefined) {
+          console.log(res.answer);
+          speak({text: res.answer })
+          setReply(res.answer)
+        }}
+        speech();
+    },[res,speak])
+
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
+
+  const Icon=()=>{
+    return(
+      <div className='container '>
+      <div className='wrapper'> 
+        <img onClick={listening ? ()=>{SpeechRecognition.stopListening(); resetTranscript(); } :()=>{SpeechRecognition.startListening(); resetTranscript();setReply("");}} className="mic" width="35px" height="35px" src={listen} alt='/'/>
+      </div>
+      </div>
+    )
+  }
+  return (
+    <div className='relative'>
+      <div className='upr-div'>
+        <div className='listenanim'>
+          {listening? <video className='vdo' src={listeningAnim} autoPlay loop /> :"" }
+        </div>
+      <p className='text'>{transcript}</p>
+      <p id='answer' style={{color:"white"}}>{reply}</p>
+      </div>
+      <Icon/>
+    </div>
+  );
+};
+export default Dictaphone;
---
+++ src/components/home.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import Dictaphone from "./Speech";
+import "./home.css"
+import 'animate.css';
+import logo from "./Sharda university logo purple.png" 
+const Home=()=>{
+    const [loading, setLoading] = useState(true);
+    useEffect(()=>{
+        setTimeout(()=>{
+            setLoading(false)
+        }, 1500)
+    },[])
+
+    if(loading){
+        return (
+            <div className="load">
+                 <div className="logo-anim">
+                 <img className='logo' src={logo} height="95px" alt='sharda logo'/>
+                 </div>
+            </div>
+        )
+    }
+    return(
+        <div className="home">
+        <div style={{width:"330px",paddingTop:"5%"}}>
+             <h1 className="animate__animated animate__slideInDown" style={{color:"white", fontWeight:"500"}}>Hello!</h1>
+             <h1 className="animate__animated animate__slideInDown" style={{color:"#A377D6",  fontWeight:"500"}}>How can I help?</h1>
+        </div> 
+        <Dictaphone/>
+        </div>
+    )
+}
+export default Home;
